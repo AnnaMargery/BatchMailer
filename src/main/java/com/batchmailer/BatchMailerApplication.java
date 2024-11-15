30f335bf56324f8f0557c5d907c5a3bff3b3a9fb
Anna Margery <amargery@splunk.com>
2024-11-16 00:15:09 +0100

    init commit

---
+++ src/main/java/com/batchmailer/BatchMailerApplication.java
@@ -0,0 +1,13 @@
+package com.batchmailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchMailerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchMailerApplication.class, args);
+    }
+
+}
