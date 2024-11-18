5eac99f679fa8f364e1292ecd668bc9974637557
Anna Margery <amargery@splunk.com>
2024-11-18 17:36:49 +0100

    updated htmls

--- src/main/java/com/batchmailer/controller/MailMessageController.java
+++ src/main/java/com/batchmailer/controller/MailMessageController.java
@@ -6,8 +6,7 @@ package com.batchmailer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MailMessageController {
