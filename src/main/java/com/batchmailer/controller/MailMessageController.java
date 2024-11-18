9936adbd8c3b8cc83c242de9c44aec262d66b9f1
Anna Margery <amargery@splunk.com>
2024-11-18 18:50:52 +0100

    added endpoints for provideMessage and receiving

--- src/main/java/com/batchmailer/controller/MailMessageController.java
+++ src/main/java/com/batchmailer/controller/MailMessageController.java
@@ -5,14 +5,34 @@
 package com.batchmailer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("messages")
 public class MailMessageController {
 
-    @GetMapping("/message")
-    public String getHome(){
-        return "message";
-    }
+  // without db- storing here
+  private String providedMessage;
+
+  @GetMapping("/")
+  public String getHome() {
+    return "message";
+  }
+
+  @PostMapping("/")
+  public String provideMessage(@RequestParam String message, Model model) {
+    this.providedMessage = message; // todo after add service and repo layers
+    model.addAttribute("message", message);
+    return "redirect:/status";
+  }
+
+  @GetMapping("/status")
+  public String getStatusPage() {
+    // todo update status page- mistakes...
+    return "status";
+  }
 }
